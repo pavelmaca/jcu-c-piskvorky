6e544bab5ef2256122bfb9feb8824796c042790d
Pavel Máca <maca.pavel@gmail.com>
2016-05-11 19:13:14 +0200

    beter algothm wip

--- src/AI.h
+++ src/AI.h
@@ -8,34 +8,102 @@
 
 #include <iostream>
 #include "Storage.h"
+#include "Coordinates.h"
 
+class AI {
+    int winningSize;
+    Storage *storage;
 
-class AI{
+    int arraySize = 0;
 
-    int countHorizontalWeight(int x, int y){
-        int inRow = 0;
+    /** našte např. všechny pětice (pokud hraji na 5) */
+    Coordinates ***generateBlocks() {
+        int leftOver = winningSize - 1;
+        int numberOfBLockInLine = storage->getSize() - leftOver;
+        arraySize = numberOfBLockInLine * 4 * storage->getSize();
 
-        Player *currentPlayer = fields[x][y];
-
-        int xBegin = x - winningSize > 0 ? x - winningSize : 0;
-        int xEnd = x + winningSize < size ? x + winningSize : size - 1;
-
-        // check newt winningSize -1 fields, end on right side of fields
-        for (int xCurrent = xBegin; xCurrent <= xEnd; ++xCurrent) {
-            if (fields[xCurrent][y] != currentPlayer) {
-                inRow = 0;
-            } else {
-                if (++inRow == winningSize) return true;
+        Coordinates ***blocks = new Coordinates **[arraySize];
+        for (int i = 0; i < arraySize; ++i) {
+            blocks[i] = new Coordinates *[winningSize];
+            for (int j = 0; j < winningSize; ++j) {
+                blocks[i][j] = NULL;
             }
         }
-        return false;
-    }
 
-public:
-    int* getNextMove(Storage *storage){
+        int c = -3;
+
         for (int x = 0; x < storage->getSize(); ++x) {
             for (int y = 0; y < storage->getSize(); ++y) {
-                if(storage->isEmpty(x, y)){
+                c += 3;
+
+                bool addHorizontal = (x < storage->getSize() - winningSize);
+                bool addVertical = (y < storage->getSize() - winningSize);
+
+                if (addHorizontal) c++;
+                if (addVertical) c++;
+                if (addHorizontal && addVertical) c++;
+
+                for (int i = 0; i < winningSize; ++i) {
+                    int g = c;
+                    if (addHorizontal) {
+                        // horizontal
+                        blocks[g++][i] = new Coordinates(x + i, y);
+                    }
+                    if (addVertical) {
+                        //vertical
+                        blocks[g++][i] = new Coordinates(x, y + i);
+                    }
+                    if (addHorizontal && addVertical) {
+                        //main diagonal
+                        blocks[g++][i] = new Coordinates(x + i, y + i);
+                    }
+                }
+            }
+        }
+
+        // slave diagonal only :(
+        for (int x = storage->getSize() - 1; x >= leftOver; --x) {
+            for (int y = storage->getSize() - 1; y >= leftOver; --y) {
+                for (int i = 0; i < winningSize; ++i) {
+                    blocks[c][i] = new Coordinates(x - i, y - i); // slave diagonal
+                }
+                c++;
+            }
+        }
+
+
+        return blocks;
+
+    }
+
+    void getValueFromBLock(Coordinates ***blocks) {
+        int attackPoints = 0;
+        int defendPoints = 0;
+
+        for (int i = 0; i < arraySize; ++i) {
+            for (int j = 0; j < winningSize; ++j) {
+                if (blocks[i][j] == NULL) continue;
+                cout << blocks[i][j]->getX() << ":" << blocks[i][j]->getY() << endl;
+            }
+            cout << "----" << endl;
+        }
+    }
+
+
+public:
+    AI(int winningSize, Storage *storage) {
+        this->winningSize = winningSize;
+        this->storage = storage;
+    }
+
+    void test() {
+        getValueFromBLock(generateBlocks());
+    }
+
+    int *getNextMove(Storage *storage) {
+        for (int x = 0; x < storage->getSize(); ++x) {
+            for (int y = 0; y < storage->getSize(); ++y) {
+                if (storage->isEmpty(x, y)) {
                     return new int[2]{x, y};
                 }
             }
@@ -43,4 +111,5 @@ public:
         //return new int[2]{0,0};
     }
 };
+
 #endif //PISKVORKY_AI_H
---
+++ src/CoordinatesGroup.h
@@ -0,0 +1,11 @@
+//
+// Created by Assassik on 11. 5. 2016.
+//
+
+#ifndef PISKVORKY_COORDINATESGROUP_H
+#define PISKVORKY_COORDINATESGROUP_H
+
+class Coordinates{
+
+};
+#endif //PISKVORKY_COORDINATESGROUP_H
